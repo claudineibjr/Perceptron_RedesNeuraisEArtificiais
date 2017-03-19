b98091c8048ee05332408bc06ad580d1ac38e4cc
Claudinei B. Junior <claudineibjr@hotmail.com>
2017-03-19 01:40:30 -0300

    Fist commit

---
+++ Perceptron/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Perceptron
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Cria um Perceptron com 2 entradas
+            Perceptron p = new Perceptron(2);
+
+            int[,] padroes = {  {0, 0},
+                                {0, 1 },
+                                {1, 0 },
+                                {1, 1 }
+                            };
+
+            int[] dj = {
+                            0,
+                            0,
+                            0,
+                            1
+                        };
+
+            float taxaDeAprendizado = 0.2f;
+
+            p.treinar(padroes, dj, taxaDeAprendizado);
+
+            int[] ent1 = { 0, 0 };
+            int[] ent2 = { 0, 1 };
+            int[] ent3 = { 1, 0 };
+            int[] ent4 = { 1, 1 };
+            Console.WriteLine("0, 0 = " + p.Y(ent1));
+            Console.WriteLine("0, 1 = " + p.Y(ent2));
+            Console.WriteLine("1, 0 = " + p.Y(ent3));
+            Console.WriteLine("1, 1 = " + p.Y(ent4));
+        }
+    }
+}
---
+++ Perceptron/Perceptron.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Perceptron{
+    class Perceptron{
+
+        double[] pesos;
+
+        public Perceptron(int _NumEntradas){
+            pesos = new double[_NumEntradas + 1];
+            pesos[0] = 0.3;
+            pesos[1] = -0.5;
+            pesos[2] = 0.2;
+            //TODO: Criar pesos aleatórios entre -1 e 1
+        }
+
+        public double somatorio(int [] entradas){
+            double _somatorio = 0;
+
+            for (int i = 0; i < pesos.Length; i++){
+                _somatorio += pesos[i] * (i == 0 ? 1 : entradas[i-1] );
+            }
+
+            return _somatorio;
+        }
+
+        public int Y(int[] entradas){
+            if (somatorio(entradas) >= 0)
+                return 1;
+            else
+                return 0;
+        }
+
+        public void treinar(int [,] padroes, int [] dj, float taxaDeAprendizado){
+            bool temErro = false;
+
+            //w(t+1) = wi(t) + n *e*xi(t)
+
+            //TODO: Atualizar pesos
+
+            for (int i = 0; i < pesos.Length; i++){
+                
+                //double _pesoAuxiliar = 
+
+            }
+
+            while (temErro){
+
+            }
+        }
+    }
+}
